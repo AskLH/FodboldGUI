ab1a63e28976c6c624e34775c63bf6e6f2419676
Markuliaso <74357747+Markuliaso@users.noreply.github.com>
2021-10-07 12:55:53 +0200

    Update MainGUI.py

--- MainGUI.py
+++ MainGUI.py
@@ -1,14 +1,14 @@
 import tkinter as ask
 window = ask.Tk()
 greeting = ask.Label(
-    text="Hello, Tkinter",
+    text="Hello, Aske",
     fg="white",
     bg="black",
     width=10,
     height=10
 )
 button = ask.Button(
-    text="Click me!",
+    text="Click Aske!",
     width=25,
     height=5,
     bg="blue",
@@ -17,4 +17,3 @@ button = ask.Button(
 button.pack()
 greeting.pack()
 window.mainloop()
-

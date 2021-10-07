157ef790c9a0904df16a8b752117ba7ace68e968
Markuliaso <74357747+Markuliaso@users.noreply.github.com>
2021-10-07 12:54:38 +0200

    Update MainGUI.py

--- MainGUI.py
+++ MainGUI.py
@@ -1,13 +1,13 @@
-import tkinter as tk
-window = tk.Tk()
-greeting = tk.Label(
+import tkinter as ask
+window = ask.Tk()
+greeting = ask.Label(
     text="Hello, Tkinter",
     fg="white",
     bg="black",
     width=10,
     height=10
 )
-button = tk.Button(
+button = ask.Button(
     text="Click me!",
     width=25,
     height=5,
